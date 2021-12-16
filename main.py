1e0cbfc98b81cef434eddadfeba9f5966e32ab12
Agamdeep Singh <agamdeep579@gmail.com>
2021-12-16 22:55:36 +0530

    Updated the code

--- main.py
+++ main.py
@@ -6,8 +6,10 @@
 # Twitter: @CoderAgam001
 # Linkdin: Agamdeep Singh
 
+# The random moudule generates a random number
 import random
 
+# Define Rock, Paper, and Scissors as ASCII arts
 rock = '''
     _______
 ---'   ____)
@@ -35,11 +37,16 @@ scissors = '''
 ---.__(___)
 '''
 
+# Print a welcome message
+print("Welcome to Rock, Paper or Scissors!\n")
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+# Get the inputs from the user
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
+# Define a random number chosen by the computer
 computer_choice = random.randint(0, 2)
 
+# If the user chose Rock, the below condition gets executed 
 if user_choice == 0:
   print(rock)
 
@@ -61,7 +68,8 @@ if user_choice == 0:
     print(scissors)
 
     print("\nYou Win")
-  
+
+# If the user chose Paper, the below condition gets executed   
 elif user_choice == 1:
   print(paper) 
 
@@ -83,8 +91,9 @@ elif user_choice == 1:
     print(scissors)
 
     print("\nYou Lose")
-  
-else:
+
+# If the user chose Scissors, the below condition gets executed   
+elif user_choice == 2:
   print(scissors)
 
   if computer_choice == 0:
@@ -105,3 +114,7 @@ else:
     print(scissors)
 
     print("\nIt's Draw")
+
+# Print this message if the user typed a number other than 0, 1 or 2
+else:
+  print("You typed an invalid number, You Lose!")
